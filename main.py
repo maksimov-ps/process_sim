b60843b0218ecd5e38ba9fabbc428721ccc9ddbd
maksimov-ps <maksimov.ps95@gmail.com>
2025-10-26 18:37:01 +0200

    backup commit with EquationOfStateInterface and ActivityModelInterfaces classes separated

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np 
 from core import Stream
-from properties import IdealGasBackend, SRK
+from properties import IdealGasBackend
 
 if __name__ == "__main__":
 
-    feed = Stream(backend = IdealGasBackend(components=('H2', 'N2', 'NH3')),
+    feed = Stream(backend = IdealGasBackend(components=('H2', 'N2', 'NH3', 'CH3OH','C2H5OH')),
                   pressure_Pa = 1e5,
                   temperature_K = 300.0,
-                  molar_composition = np.array([0.7, 0.3, 0.0]),
+                  molar_composition = np.array([0.7, 0.3, 0.0, 0.0, 0.0]),
                   molar_flow_mol_s = 1.0)
     
     print(feed.density_SI())
--- properties.py
+++ properties.py
@@ -3,27 +3,69 @@ from typing import Protocol
 from chemicals.elements import molecular_weight as MW_data_import 
 from chemicals.elements import nested_formula_parser as MW_data_parser
 
-class PropertyPackageInterface(Protocol): 
+import thermo
+
+class EquationOfStateInterface(Protocol): 
     
-    """Common interface for equation-of-state backends."""
+    """ Common interface for equation-of-state backends. """
     name: str
     components: tuple[str, ...]       
     
+    def get_fugacity_coefs(self,
+                           temperature_K: float, 
+                           pressure_Pa: float, 
+                           molar_composition: np.ndarray) -> np.ndarray: ...
+
     def get_compressibility_factor(self, 
-              temperature_K: float, 
-              pressure_Pa: float, 
-              molar_composition: np.ndarray) -> float: ...
+                                   temperature_K: float,
+                                   pressure_Pa: float,
+                                   molar_composition: np.ndarray) -> float: ...
     
-    def get_density_SI(self, 
-              temperature_K: float, 
-              pressure_Pa: float, 
-              molar_composition: np.ndarray) -> float: ...
+    def get_density_SI(self,
+                       temperature_K: float,
+                       pressure_Pa: float,
+                       molar_composition: np.ndarray) -> float: ...
+    
+
+class ActivityModelInterface(Protocol):
+
+    """ Common interface for activity model backends. """
+    name: str
+    components: tuple[str, ...]       
+    
+    def get_activity(self, 
+                     temperature_K: float,
+                     pressure_Pa: float,
+                     molar_composition: np.ndarray) -> np.ndarray: ...
+
+
+
+
+
+class GammaPhiPackage():
+
+    """ Property package to calculate VLE using activity and EOS models. """
+    def __init__(self, 
+                 eos_backend: EquationOfStateInterface,
+                 activity_model_backend: ActivityModelInterface):
+        self.eos_backend = eos_backend
+        self.activity_model_backend = activity_model_backend
+        self.components = eos_backend.components
+
+    def TP_flash(self): 
+
+        " Isothermal-isobaric flash calculation. "
+
+
+
+
+        return None
+
 
 
 class IdealGasBackend:
 
     """ Ideal gas property package backend. """
-
     name = 'IdealGas'
     
     def __init__(self, 
@@ -41,20 +83,20 @@ class IdealGasBackend:
                        pressure_Pa: float,
                        molar_composition: np.ndarray) -> float:
         
-        molecular_weight_vec_g_mol = np.array([MW_data_import(MW_data_parser(f)) for f in self.components], dtype=float) 
+        molecular_weight_vec_g_mol = np.array([MW_data_import(MW_data_parser(f)) for f in self.components]) 
         
-        R = 8.314462618 # J/(mol·K)
-        P = pressure_Pa
-        T = temperature_K
-        MW = np.dot(molecular_weight_vec_g_mol, molar_composition) / 1000 # kg/mol
-        rho = P * MW / (R * T) 
+        R: float = 8.314462618 # J/(mol·K)
+        P: float = pressure_Pa
+        T: float = temperature_K
+        MW: float = np.dot(molecular_weight_vec_g_mol, molar_composition) / 1000 # kg/mol
+        rho: float = P * MW / (R * T) 
 
-        density_SI = rho 
+        density_SI: float = rho 
 
         return density_SI
     
 
-class SRK:
+class SoaveRedlichKwongBackend:
     name = 'SRK'
     
     def __init__(self, components: tuple[str, ...]):
